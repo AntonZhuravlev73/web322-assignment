4c7c92d7e9b4456bba7ecb6b58371c0a960cb3d0
Anton Zhuravlev <azhuravlev@myseneca.ca>
2024-05-21 13:27:55 -0400

    modified server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+/**
+* WEB322 - Assignment 1
+* I declare that this assignment is my own work in accordance
+with Seneca Academic Policy.
+* No part of this assignment has been copied manually or
+electronically from any other source
+* (including web sites, friends gpt or otherwise) or
+distributed to other students.
+* I understand that if caught doing so, I will receive zero on
+this assignment and possibly
+* fail the entire course.
+* Name: Anton Zhuravlev
+* Student ID: 108568239
+* Date: 5/21/2024
+* Vercel Web App URL:
+* GitHub Repository URL: https://github.com/AntonZhuravlev73/web322-assignment
+**/
+console.log("Anton Zhuravlev - 108568239");
